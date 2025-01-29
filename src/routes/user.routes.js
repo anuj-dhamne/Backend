348f582ec56965c47f83f6e5594e4e24491727f7
anuj-dhamne <anujdhamne10@gmail.com>
2025-01-29 17:33:41 +0530

    adding new routes for userControllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,29 +1,44 @@
-import {Router} from "express";
-import { registerUser,loginUser,logoutUser, refreshAccessToken} from "../controllers/user.controllers.js";
+import { Router } from "express";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvator, updateUserCoverImage, getUserCahnnelprofile, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
-const router=Router();
+const router = Router();
 
 // applying upload middleware 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avator",
-            maxCount:1
+            name: "avator",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ])
-    ,registerUser)
+    , registerUser)
 
 router.route("/login").post(loginUser)
 
 // secured notes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-details").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avator").patch(verifyJWT, upload.single("avator"), updateUserAvator);
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserCahnnelprofile)
+
+router.route("/watchHistory").get(verifyJWT, getWatchHistory);
+
 export default router;
 
